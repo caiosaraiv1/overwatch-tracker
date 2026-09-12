d009c72bc7180c5fb5c7f3e8b1de8a303b4bd3a6
caiosaraiv1 <caio.ariel@outlook.com>
2026-09-12 20:14:00 -0300

    chore: initialize project structure

---
+++ src/main/java/com/caio/overwatch_tracker/OverwatchTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.caio.overwatch_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OverwatchTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OverwatchTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/caio/overwatch_tracker/OverwatchTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caio.overwatch_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OverwatchTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
