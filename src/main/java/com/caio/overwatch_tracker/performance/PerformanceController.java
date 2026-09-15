4e91d2213d84d5246150b9d73f740f04edbb1451
caiosaraiv1 <caio.ariel@outlook.com>
2026-09-15 18:22:55 -0300

    feat(performance): implement nested endpoints for add, update and delete

--- src/main/java/com/caio/overwatch_tracker/performance/PerformanceController.java
+++ src/main/java/com/caio/overwatch_tracker/performance/PerformanceController.java
@@ -1,4 +1,37 @@
 package com.caio.overwatch_tracker.performance;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class PerformanceController {
+
+    private final PerformanceService performanceService;
+
+    public PerformanceController(PerformanceService performanceService) {
+        this.performanceService = performanceService;
+    }
+
+    @PostMapping("/matches/{id}/performances")
+    public ResponseEntity<Performance> addPerformance(@RequestBody PerformanceRequest performanceRequest, @PathVariable("id") Long matchId) {
+        Performance save = performanceService.addPerformance(matchId, performanceRequest);
+        return ResponseEntity.status(201).body(save);
+    }
+
+    @PatchMapping("/matches/{id}/performances/{performanceId}")
+    public ResponseEntity<Performance> updatePerformance(@RequestBody PerformanceRequest performanceRequest, @PathVariable("id") Long matchId, @PathVariable("performanceId") Long performanceId) {
+        Performance save = performanceService.updatePerformance(matchId, performanceId, performanceRequest);
+        return ResponseEntity.status(200).body(save);
+    }
+
+    @DeleteMapping("/matches/{id}/performances/{performanceId}")
+    public ResponseEntity<Void> deletePerformance(@PathVariable("id") Long matchId, @PathVariable("performanceId") Long performanceId) {
+        performanceService.deletePerformance(matchId, performanceId);
+        return ResponseEntity.noContent().build();
+    }
 }
--- src/main/java/com/caio/overwatch_tracker/performance/PerformanceService.java
+++ src/main/java/com/caio/overwatch_tracker/performance/PerformanceService.java
@@ -1,4 +1,87 @@
 package com.caio.overwatch_tracker.performance;
 
+import com.caio.overwatch_tracker.hero.Hero;
+import com.caio.overwatch_tracker.hero.HeroRepository;
+import com.caio.overwatch_tracker.match.Match;
+import com.caio.overwatch_tracker.match.MatchRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class PerformanceService {
+
+    private final PerformanceRepository performanceRepository;
+    private final MatchRepository matchRepository;
+    private final HeroRepository heroRepository;
+
+    public PerformanceService(PerformanceRepository performanceRepository, MatchRepository matchRepository, HeroRepository heroRepository) {
+        this.performanceRepository = performanceRepository;
+        this.matchRepository = matchRepository;
+        this.heroRepository = heroRepository;
+    }
+
+    public Performance addPerformance(Long matchId, PerformanceRequest request) {
+        Match match = matchRepository.findById(matchId)
+                .orElseThrow(() -> new RuntimeException("Match ID not found."));
+
+        Hero hero = heroRepository.findById(request.getHeroId())
+                .orElseThrow(() -> new RuntimeException("Hero ID not found."));
+
+        Performance performance = new Performance();
+
+        performance.setMatch(match);
+        performance.setHero(hero);
+
+        performance.setHeroOrder(request.getHeroOrder());
+        performance.setTimePlayed(request.getTimePlayed());
+        performance.setEliminations(request.getEliminations());
+        performance.setAssists(request.getAssists());
+        performance.setDeaths(request.getDeaths());
+        performance.setDamageDealt(request.getDamageDealt());
+        performance.setHealingDone(request.getHealingDone());
+        performance.setDamageMitigated(request.getDamageMitigated());
+
+        performance = performanceRepository.save(performance);
+
+        return performance;
+    }
+
+    public Performance updatePerformance(Long matchId, Long performanceId, PerformanceRequest request) {
+        Performance performance = performanceRepository.findById(performanceId)
+                .orElseThrow(() -> new RuntimeException("Performance ID not found."));
+
+        if (!performance.getMatch().getId().equals(matchId)) {
+            throw new RuntimeException("Performance " + performanceId + " does not belong to Match " + matchId);
+        }
+
+        Hero hero = heroRepository.findById(request.getHeroId())
+                .orElseThrow(() -> new RuntimeException("Hero ID not found."));
+
+        performance.setHero(hero);
+
+        performance.setHeroOrder(request.getHeroOrder());
+        performance.setTimePlayed(request.getTimePlayed());
+        performance.setEliminations(request.getEliminations());
+        performance.setAssists(request.getAssists());
+        performance.setDeaths(request.getDeaths());
+        performance.setDamageDealt(request.getDamageDealt());
+        performance.setHealingDone(request.getHealingDone());
+        performance.setDamageMitigated(request.getDamageMitigated());
+
+        performance = performanceRepository.save(performance);
+
+        return performance;
+    }
+
+    public void deletePerformance(Long matchId, Long performanceId) {
+        Performance performance = performanceRepository.findById(performanceId)
+                .orElseThrow(() -> new RuntimeException("Performance ID not found."));
+
+        if (!performance.getMatch().getId().equals(matchId)) {
+            throw new RuntimeException("Performance " + performanceId + " does not belong to Match " + matchId);
+        }
+
+        performanceRepository.delete(performance);
+    }
+
+
 }
