78a4b5ccf54429b6c9e31c0a481aa58cdeba3420
caiosaraiv1 <caio.ariel@outlook.com>
2026-09-19 18:23:37 -0300

    feat(db): add flyway migrations for schema creation and hero seeding

---
+++ src/main/resources/db/migration/V2__seed_heroes.sql
@@ -0,0 +1,17 @@
+INSERT INTO hero (name, role)
+VALUES ('Ana', 'SUPPORT');
+
+INSERT INTO hero (name, role)
+VALUES ('Juno', 'SUPPORT');
+
+INSERT INTO hero (name, role)
+VALUES ('Junkrat', 'DAMAGE');
+
+INSERT INTO hero (name, role)
+VALUES ('Mei', 'DAMAGE');
+
+INSERT INTO hero (name, role)
+VALUES ('Roadhog', 'TANK');
+
+INSERT INTO hero (name, role)
+VALUES ('Orisa', 'TANK');
